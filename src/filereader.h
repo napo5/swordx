50b39cc5d733631095077de80a14878e96feca4f
Luca Pretini <lucapretini97@gmail.com>
2018-10-13 12:19:08 +0200

    Delete filereader.h

--- src/filereader.h
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-FILE* open_file(char* path);
-FILE* create_file(char* path);
-char *stringSaver(FILE *file, int fileSize);
-int fileLenght(FILE* fp);
-void folderPrint( char * path);
-
